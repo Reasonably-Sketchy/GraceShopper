212ebbf338f0ae6bc910b5b410049f7c00c84731
Nick Swanson <nickjswanson@yahoo.com>
2021-04-06 15:20:04 -0700

    CREATE - added some api functionality where there was none

---
+++ TeamContract.md
@@ -0,0 +1,31 @@
+FlimFlam Shoppe(working title)
+A reasonably sketchy shopping website for those who like to pay more for little to nothing!
+
+_This doc describes our team social contract for this project_
+
+Daily Process{
+-Evening Standup: Assessing jobs that need to be done and assigning/volenteering for jobs.
+-Pairs during difficult tasks & availability to help with code when others are struggling. All members should be online at some point on a given day to be able to help and assist with another to create code.
+-6:00pm every day standup meetings
+-regularly check for pull requests each day and review code each time. Us the review methods introduced in class, using shorthand descriptions within the title to display what type of pull request it is.
+}
+
+Team Expectations{
+-Each of us reserves the right to time off on holidays (like Easter coming up), and to visit family with a heads up to the team before hand.
+-If you have an issue feel free to slack another worker to ask for help
+-Each person will work on all aspects of the project, we will rotate workers working on the back end and front end each review period to force further moveablility.
+-Speak up, and allow others to do so
+-Messege on slack when you are working on something
+}
+
+Issues{
+-If the group is divided on an issue we will:
+----list pros and cons
+----discuss and look for a third option
+----vote on which we will do if need be
+-If a group member is frustrated
+----take a break
+----try to talk constructively about actions
+----respect each other
+----bring in a instructor to mediate if need be
+}
--- api/products.js
+++ api/products.js
@@ -1,6 +1,60 @@
-const express = require('express');
+const express = require("express");
 const productsRouter = express.Router();
+const { requireUser } = require("./utils");
+const { client, updatePost, getPostById } = require("../db");
+client.connect();
 
 // const {} = require('../db');
+
+productsRouter.use((req, res, next) => {
+  console.log("A request is being made to /products");
+  next();
+});
+
+const { getAllProducts } = require("../db");
+
+productsRouter.delete("/:postId", requireUser, async (req, res, next) => {
+  try {
+    const post = await getProductById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(
+        post
+          ? {
+              name: "UnauthorizedUserError",
+              message: "You cannot delete a post which is not yours",
+            }
+          : {
+              name: "ProductNotFoundError",
+              message: "That product does not exist",
+            }
+      );
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+productsRouter.get("/", async (req, res) => {
+  try {
+    const allProducts = await getAllProducts();
+
+    const products = allProducts.filter((product) => {
+      return product.active;
+      //   || (req.user && post.author.id === req.user.id)
+    });
+
+    res.send({
+      products,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 module.exports = productsRouter;
